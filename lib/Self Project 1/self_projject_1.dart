297b54444eb0297c173943eb4b8b6939db297745
OmerAsik <faruk.asikoglu@gmail.com>
2022-05-15 18:18:30 +0300

    Create self_projject_1.dart

---
+++ lib/Self Project 1/self_projject_1.dart
@@ -0,0 +1,26 @@
+void main(){
+  int islemNo = 1;
+  String aTarih = "20/01/2001";
+  String vTarih = "18/08/2008";
+
+  String ogrAd = "Hasan";
+  String ogrSoyad = "Tahsin";
+  int ogrNo = 128;
+  String cinsiyet = 'e';
+  String dTarih = "11/06/1990";
+  String sinif = "1-A";
+
+  String kitapAdi = 'Alice Harikalar Diyarında';
+  int kitapNo = 7;
+  int isbNo = 1234567890123;
+  int sayfaSayisi = 688;
+  int puan = 100;
+
+  String turAdi = "Macera";
+  int turNo = 2;
+
+  String yazarAd =  "James Matthew";
+  String yazarSoyad =  "Barrie";
+  int yazarNo = 8;
+
+}
